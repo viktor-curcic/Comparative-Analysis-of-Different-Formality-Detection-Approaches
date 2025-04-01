61e7c9c9355fcc61bcabe14880eccc4e89b6d087
viktor-curcic <viktorcurcic@hotmail.com>
2025-04-01 11:38:10 +0200

    pair_generation.py

--- scripts/data_processing/pair_generation.py
+++ scripts/data_processing/pair_generation.py
@@ -13,4 +13,4 @@ for line in pairs.split("\n"):
         data.append({"text": line.replace("[informal]", "").strip(), "label": 0})   
 
 df = pd.DataFrame(data)  
-df.to_csv("synthetic_formality_data.csv", index=False)   
+df.to_csv(os.path.join(current_dir, "../../data/synthetic_formality_data.csv"), index=False)   
