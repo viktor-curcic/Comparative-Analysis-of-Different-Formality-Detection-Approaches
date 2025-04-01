e95a003beab4f4f671a6caa3dedc0b31b6f0366c
viktor-curcic <viktorcurcic@hotmail.com>
2025-04-01 12:07:54 +0200

    textblobb.py

--- scripts/models/textblobb.py
+++ scripts/models/textblobb.py
@@ -50,7 +50,7 @@ print("Confusion Matrix:\n", confusion_matrix(y_true, y_pred))
 
 RocCurveDisplay.from_predictions(y_true, y_scores)
 plt.title('ROC Curve')
-plt.savefig('textblob_roc_curve.png')
+plt.savefig(os.path.join(current_dir, "../../results/plots/textblob_roc_curve.png"))
 plt.close()
 
 results_df = test_df.copy()
