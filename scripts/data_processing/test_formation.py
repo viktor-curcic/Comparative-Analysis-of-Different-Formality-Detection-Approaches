a733173a72525bf8ddfbaa713bd4f01907db013b
viktor-curcic <viktorcurcic@hotmail.com>
2025-04-01 11:40:39 +0200

    test_formation.py

--- scripts/data_processing/test_formation.py
+++ scripts/data_processing/test_formation.py
@@ -8,6 +8,6 @@ df = pd.read_csv(os.path.join(current_dir, "../../data/synthetic_formality_data.
 train_df, temp_df = train_test_split(df, test_size=0.3, random_state=42)
 val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
 
-train_df.to_csv("train.csv", index=False)
-val_df.to_csv("val.csv", index=False)
-test_df.to_csv("test.csv", index=False)
+train_df.to_csv(os.path.join(current_dir, "../../data/processed/train.csv"), index=False)
+val_df.to_csv(os.path.join(current_dir, "../../data/processed/val.csv"), index=False)
+test_df.to_csv(os.path.join(current_dir, "../../data/processed/test.csv"), index=False)
